8e5abae45bab93ee725242d5357622a5857fac31
523370910102 <fztym@sjtu.edu.cn>
2024-01-28 21:18:32 +0100

    feat. PostAutoGenerator

---
+++ PostAutoGenerator.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+namespace fs = std::filesystem;
+
+int main() {
+    const fs::path cwd = fs::current_path();  // get the current path
+    int fid = -1;                             // skip this file and the markdown file
+
+    std::ofstream outfile(cwd.filename().string() + ".md", std::ios::out);
+
+    outfile << "---" << std::endl;
+    outfile << "layout: post" << std::endl;
+    outfile << "title: " << cwd.filename().string() << std::endl;
+    outfile << "---" << std::endl
+            << std::endl;
+
+    for (auto const& dir_entry : fs::directory_iterator{cwd}) {  // iterate the dir
+        outfile << "![pic" << ++fid << "]"
+                << "({{site.baseurl}}/" 
+                << cwd.parent_path().filename().string() << "/"
+                << cwd.filename().string() << "/"
+                << dir_entry.path().filename().string() << ")" << std::endl
+                << std::endl;
+    }
+    outfile.close();
+    system("rm PostAutoGenerator");  // delete the file itself
+    return 0;
+}
